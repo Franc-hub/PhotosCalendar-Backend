ab86808bba3f2025047a6f1963ff6be63206f086
Franc Cosp Meya <franccospmeyaeso@gmail.com>
2021-10-28 10:26:31 +0200

    creating

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # PhotosCalendar-Backend
+
+
+Commands:
+
+source ./venv/bin/activate  
+#
+to inizalazing the virtual env with the dependencies 
+
+python src/app.py
+#
+to start the project
--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,Response
 from flask_pymongo import PyMongo
 from werkzeug.security import check_password_hash, generate_password_hash
+from bson import json_util
+from werkzeug.wrappers import response
+
+
 app = Flask(__name__)
 app.config['MONGO_URI']='mongodb://localhost/pythonmongo'
 
@@ -10,11 +14,18 @@ mongo = PyMongo(app)
 def test():
     return {"Message":"ok"}
 
-@app.error_handler(404)
+@app.errorhandler(404)
 def not_found(error = None):
-    return jsonify(
-        {"message" : "resource not found" + request.url,
-         "status":   404 }) 
+    res =  jsonify(
+        {"message" : "Resource Not found" + request.url ,
+         "status":   404 }
+         )
+    
+
+    return res, 404 
+
+# CRUD USER
+
 
 @app.route("/users",methods=['POST'])
 def create_user():
@@ -22,17 +33,24 @@ def create_user():
     email = request.json['email']
     password = request.json['password']
     
-    if username and email and password  is not None:
+    if username and email and password  is not None and mongo.db.users.find():
+        
         hash_password = generate_password_hash(password)
         id = mongo.db.users.insert({
                  'username': username,'email':email, 'password':hash_password
         })
-   
+    
         return jsonify (id = str(id), username = username, password=password, email  =email) ,201
-        
+    
     else:
-        {"message":"You didn't complete all the data"}
+        return not_found()
 
+@app.route("/users",methods=['GET'])
+def get_all_users():
+
+    users = mongo.db.users.find()
+    res = json_util.dumps(users)
+    return Response(res,mimetype='application/json'), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
